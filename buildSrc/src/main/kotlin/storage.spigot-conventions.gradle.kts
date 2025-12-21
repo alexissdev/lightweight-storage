9e5a5a9471bef9a47db87b7597b915362c7df856
alexissdev <costaaalexis@gmail.com>
2025-12-21 12:52:13 -0300

    deps: fix the spigot and bungeecord-chat error

--- buildSrc/src/main/kotlin/storage.spigot-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/storage.spigot-conventions.gradle.kts
@@ -3,6 +3,13 @@ plugins {
 }
 
 repositories {
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven {
+        name = "Spigot"
+        url = uri("https://hub.spigotmc.org/nexus/content/groups/public/")
+    }
+
+    maven {
+        name = "BungeeCord"
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
 }
